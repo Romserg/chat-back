8a1524679dfc4cb8d05bdd387fc765cff1ed7011
Romserg-Do-IT <s.romankov@do-it.co>
2018-12-05 16:25:04 +0200

    *added posibility for save new users into database *realized encryption of password

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const Joi = require('joi');
 const HttpStatus = require('http-status-codes');
 const User = require('../models/userModels');
 const Helpers = require('../Helpers/helper');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
     async CreateUser(req, res) {
         const schema = Joi.object().keys({
@@ -11,6 +13,7 @@ module.exports = {
         });
 
         const { error, value } = Joi.validate(req.body, schema);
+        
         console.log(value);
         if (error && error.details) {
             return res.status(HttpStatus.BAD_REQUEST).json({ message: error.details });
@@ -18,12 +21,30 @@ module.exports = {
 
         const userEmail = await User.findOne({ email: Helpers.lowerCase(req.body.email) });
         if (userEmail) {
-            res.status(HttpStatus.CONFLICT).json({ message: 'Email already exist' });
+            return res.status(HttpStatus.CONFLICT).json({ message: 'Email already exist' });
         };
 
         const userName = await User.findOne({ username: Helpers.firstLetterUpper(req.body.username) });
         if (userName) {
-            res.status(HttpStatus.CONFLICT).json({ message: 'Username already exist' });
+            return res.status(HttpStatus.CONFLICT).json({ message: 'Username already exist' });
         };
+
+        return bcrypt.hash(value.password, 10, (err, hash) => {
+            if (err) {
+                return res.status(HttpStatus.BAD_REQUEST).json({ message: 'Error hashing password' });
+            }
+            const body = {
+                username: Helpers.firstLetterUpper(value.username),
+                email: Helpers.lowerCase(value.email),
+                passoword: hash
+            };
+            User.create(body)
+                .then(user => {
+                    res.status(HttpStatus.CREATED).json({message: 'User created successfully', user});
+                })
+                .catch(err => {
+                    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'});
+                })
+        });
     }
 }
