75aa13e65ea73ce57de9e1a977cc25ba5de210b1
Romserg-Do-IT <s.romankov@do-it.co>
2018-12-05 14:59:25 +0200

    add register route

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const app = express();
 
 const dbConfig = require('./config/secret');
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(logger('dev'));
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.url, { useNewUrlParser: true });
+mongoose.connect(
+    dbConfig.url,
+    { useNewUrlParser: true }
+);
+
+const auth = require('./routes/authRoutes');
+app.use('/api/chat', auth);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
