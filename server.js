6b552154ab8ca25235a883e1b47e47b3f9eeb7bb
Romserg <serg.romankov@gmail.com>
2018-12-09 22:30:35 +0200

    it is possible to send cors request

--- server.js
+++ server.js
@@ -3,10 +3,31 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const auth = require('./routes/authRoutes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 const dbConfig = require('./config/secret');
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET',
+        'POST',
+        'DELETE',
+        'PUT',
+        'OPTIONS'
+    );
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-type, Accept, Authorization'
+    );
+    next();
+});
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
